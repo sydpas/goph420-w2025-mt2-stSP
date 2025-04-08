50feb654d2712446e377a9756acd7901a6048be8
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-04-08 10:08:25 -0600

    made my 3 beautiful graphs

--- main/rho_vp_driver.py
+++ main/rho_vp_driver.py
@@ -9,7 +9,7 @@ def main():
     rho = data[:, 0]  # density data
     vp = data[:, 1]  # p-wave velocity data
 
-    # plotting...
+    # plotting original data...
     plt.grid()
     plt.plot(rho, vp, 'ro', markersize=4)
     plt.xlabel("Density [g/cm^3]")
@@ -18,25 +18,44 @@ def main():
     plt.savefig('../figures/magnitude_vs_time.png', dpi=300)
     plt.show()
 
+    # linearizing (2b)...
+    y = np.log10(vp)  # linearize the relationship
+    Z = np.vstack((np.ones_like(rho), rho)).T  # design matrix for regression
 
-    # linearizing (2b)
-    m = rho
-    n = vp  # array of zeros same size as m
-
-    y = np.log10(n)  # linearize the relationship
-    Z = np.vstack((np.ones_like(m), m)).T  # design matrix for regression
-
-    a, e, rsq = multi_regress(y, Z)  # use least square regression to find coeffs, residuals, and R^2
+    a, e, rsq = multi_regress(y, Z)
 
     log_n = Z @ a  # linearized vp values
 
     # plotting...
     plt.grid()
-    plt.plot(m, log_n, 'bo', markersize=2)  # density vs log
+    plt.plot(rho, log_n, 'b--', markersize=2, label="Fitted")  # density-x vs log(vp)-y
+    plt.plot(rho, y, 'ro', markersize=4, label="Log Data")
+    plt.xlabel("Density [g/cm^3]")
+    plt.ylabel("Log of P-Wave Velocity [m/s]")
+    plt.legend()
+    plt.title("Linear Regression: log(Vp) versus Density")
+    plt.savefig('../figures/linear_magnitude_vs_time.png', dpi=300)
+    plt.show()
+
+    # now we fit the model in OG
+    log_V0, k = a
+    V0 = 10 ** log_V0  # undo the log to get actual V0
+    vp_fitted = V0 * rho ** k  # equation from midterm
+
+    print(f"V0 = {V0}")
+    print(f"k = {k}")
+    print(f"R squared = {rsq}")
+
+    # plotting original data with model fit...
+    plt.figure(figsize=(10, 6))
+    plt.grid()
+    plt.plot(rho, vp_fitted, 'b--', markersize=2, label="Fitted Line")  # density-x vs log(vp)-y
+    plt.plot(rho, vp, 'ro', markersize=4, label="Original Data")
     plt.xlabel("Density [g/cm^3]")
     plt.ylabel("P-Wave Velocity [m/s]")
-    plt.title("Linear Regression: P-Wave Velocity as a Function of Density")
-    plt.savefig('../figures/linear_magnitude_vs_time.png', dpi=300)
+    plt.legend()
+    plt.title("Best Fit Model")
+    plt.savefig('../figures/best_fit.png', dpi=300)
     plt.show()
 
 
