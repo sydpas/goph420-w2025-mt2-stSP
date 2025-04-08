c900eccd1a9f1bbde8477f44f04ae4b4be3dbe0e
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-04-08 17:20:04 -0600

    graphs r beautiful again!!!!!!!!

--- main/rho_vp_driver.py
+++ main/rho_vp_driver.py
@@ -17,9 +17,8 @@ def main():
     plt.plot(rho, vp, 'ro', markersize=6, alpha=0.6, label="Original Data")
     plt.xlabel("Density [g/cm³]", fontsize=12)
     plt.ylabel("P-Wave Velocity [m/s]", fontsize=12)
-    plt.title("P-Wave Velocity vs Density", fontsize=14)
-    plt.legend()
-    plt.savefig('../figures/magnitude_vs_time.png', dpi=300)
+    plt.title("Original Data", fontsize=16, fontweight='bold')
+    plt.savefig('../figures/og_plot.png', dpi=300)
     plt.show()
 
     # linearizing (2b)...
@@ -30,14 +29,6 @@ def main():
 
     log_n = Z @ a  # linearized vp values
 
-    # now we fit the model
-    k = a[1]
-    v0 = np.exp(a[0])  # undo the log to get actual V0
-
-    print(f"V0 = {v0}")
-    print(f"k = {k}")
-    print(f"R squared = {rsq}")
-
     # plotting log-linear fit...
     plt.figure(figsize=(10, 6))
     plt.grid()
@@ -45,17 +36,41 @@ def main():
     plt.plot(rho, y, 'ro', markersize=6, alpha=0.6, label="Log Data")
     plt.xlabel("Density [g/cm³]", fontsize=12)
     plt.ylabel("log(P-Wave Velocity [m/s])", fontsize=12)
+    plt.title('Linearized Data With Regression Line', fontsize=16, fontweight='bold')
     plt.legend()
-    plt.savefig('../figures/best_fit.png', dpi=300)
+    plt.savefig('../figures/linear_with_regress.png', dpi=300)
     plt.show()
 
+
+    # now we fit the model
+    k = a[1]
+    v0 = np.exp(a[0])  # undo the log to get actual V0
+    vp_new = v0 * np.exp(k * rho)
+
+    index = np.argsort(rho)
+    rho_sorted = rho[index]
+    vp_sorted = vp_new[index]
+
     # plotting original data with exponential...
-    plt.title(rf"Linear Regression of $V_p = {v0:.2f} \cdot e^{{\rho \times {k:.2f}}}$", fontsize=14)
+    plt.figure(figsize=(10, 6))
+    plt.grid()
+    plt.plot(rho_sorted, vp_sorted, 'b--', markersize=2, label="Fitted Line")
+    plt.plot(rho, vp,'ro', markersize=6, alpha=0.6, label="Original Data")
     plt.text(0.20, 0.60, f'$R^2$ = {rsq:.8f}', transform=plt.gca().transAxes,
              fontsize=12, bbox=dict(facecolor='white'))
+    plt.text(0.20, 0.70, rf'$V_p = {v0:.2f} \cdot e^{{\rho \times {k:.2f}}}$', transform=plt.gca().transAxes,
+             fontsize=12, bbox=dict(facecolor='white'))
     # transform is to place text relative to the axes, not data
+    plt.xlabel("Density [g/cm³]", fontsize=12)
+    plt.ylabel("log(P-Wave Velocity [m/s])", fontsize=12)
+    plt.title("Original Data with Regression Line", fontsize=16, fontweight='bold')
+    plt.legend()
+    plt.savefig('../figures/og_with_exp.png', dpi=300)
+    plt.show()
 
-
+    print(f"V0 = {v0}")
+    print(f"k = {k}")
+    print(f"R squared = {rsq}")
 
 
 if __name__ == "__main__":
